e4b14cc33856bda8b7f12b79cfe2fb66cdd86b7a
ThunderkeX <thunderkex@gmail.com>
2024-10-03 12:59:07 +0700

    setup !

---
+++ setup_personal.sh
@@ -0,0 +1,24 @@
+# Ubuntu 23 up
+LATEST_MAKE_VERSION="4.3"
+sudo apt-fast install software-properties-common -y
+sudo apt-fast update
+sudo apt-fast install lsb-release -y
+sudo apt-fast install git ccache automake flex lzop bison gperf build-essential zip zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven libssl-dev pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl libxml-simple-perl bc libc6-dev-i386 libx11-dev libgl1-mesa-dev xsltproc unzip device-tree-compiler libncurses5-dev curl python-is-python3
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt-fast update
+sudo apt-fast install -y gh
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo chmod 644 /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo systemctl restart udev
+if [[ "$(command -v make)" ]]; then
+    makeversion="$(make -v | head -1 | awk '{print $3}')"
+    if [[ ${makeversion} != "${LATEST_MAKE_VERSION}" ]]; then
+        echo "Installing make ${LATEST_MAKE_VERSION} instead of ${makeversion}"
+        bash "$(dirname "$0")"/make.sh "${LATEST_MAKE_VERSION}"
+    fi
+fi
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
